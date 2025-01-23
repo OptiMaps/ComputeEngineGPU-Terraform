fcd01dcb2b24e6ade8d668587d0d2726c406f47d
falconlee236 <falconlee236@cau.ac.kr>
2025-01-23 20:47:17 +0900

    add output.tf

--- src/output.tf
+++ src/output.tf
@@ -2,3 +2,15 @@
 output "bucket_name" {
     value = google_storage_bucket.artifact_bucket.name
 }
+
+output "zone_name" {
+    value = var.zone
+}
+
+output "host_name" {
+    value = "training-worker-gpu-instance"
+}
+
+output "project_name" {
+    value = var.project
+}
